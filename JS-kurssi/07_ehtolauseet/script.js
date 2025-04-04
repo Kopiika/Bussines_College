8c4726b9a56e80fa544a9e57a40677eb602992f9
Eleonora <eleonora.kopiika@gmail.com>
2025-04-04 09:23:44 +0300

    wip

--- JS-kurssi/07_ehtolauseet/script.js
+++ JS-kurssi/07_ehtolauseet/script.js
@@ -8,6 +8,24 @@ function checkAdult(age) {
 checkAdult(11);
 checkAdult(21);
 
+//ehtolause 01 Ternary operator
+
+function checkAdult1(age) {
+	return age>=18 ? "Olet aikuinen": null;
+}
+console.log(checkAdult1(20));
+
+
+
+
+// ehtolause 02 Ternary operator
+
+function checkForFox1(hasFox) {
+	return hasFox ? "Sinulla on lemmikkikettu!" : "Ei havaittu kettua!";
+}
+console.log(checkForFox1(true));
+console.log(checkForFox1(false));
+
 // ehtolause 02
 function checkForFox(hasFox) {
 	if (hasFox) {
@@ -20,11 +38,24 @@ console.log(checkForFox(true));
 console.log(checkForFox(false));
 
 
+
+
+
+// ehtolause 03 Ternary operator
+
+function getGrade1(grade) {
+	return grade>=90 ? "Arvosana Kiitettävä" :
+			 grade>=80 ? "Arvosana Hyvä" :
+			"Avosana Tyydyttävä";
+}
+console.log(getGrade1(75));
+console.log(getGrade1(85));
+
 // ehtolause 03
 function getGrade(grade) {
 	if (grade>=90) {
 		return "Arvosana Kiitettävä";
-	} else if (grade<=89, grade>=80) {
+	} else if (grade>=80) {
 		return "Arvosana Hyvä";
 	} else {
 		return "Avosana Tyydyttävä";
@@ -33,6 +64,19 @@ function getGrade(grade) {
 console.log(getGrade(85));
 console.log(getGrade(75));
 console.log(getGrade(95));
+
+
+
+
+
+// ehtolause 04 Ternary operator
+
+function canVote1(age) {
+	return age >= 18 ? true : false;
+ }
+console.log(canVote1(18) ? "Saat äänestää" : "Et saa vielä äänestää");
+console.log(canVote1(15) ? "Saat äänestää" : "Et saa vielä äänestää");
+
 
 // ehtolause 04
 function canVote(age) {
@@ -50,5 +94,7 @@ if (canVote(19)){
 } else {
 	console.log("Et saa vielä äänestää");
 }
+
+
 
 
