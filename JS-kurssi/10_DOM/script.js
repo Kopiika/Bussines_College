e2bc3320e467eddd4b016b2081f201c2a7e3fe77
Eleonora <eleonora.kopiika@gmail.com>
2025-04-11 13:05:44 +0300

    DOM exercise

--- JS-kurssi/10_DOM/script.js
+++ JS-kurssi/10_DOM/script.js
@@ -1,5 +1,40 @@
-//У цьому прикладі обробник події додається до кнопки з ідентифікатором myButton. Коли кнопку натискають, анонімна функція показує вікно сповіщення.
+//DOM 01A
+document.getElementById("press").addEventListener("click", function () {
+	document.getElementById("content").textContent = "Hei, maailma!";
+});
 
-document.getElementById("myButton").addEventListener("click", function () {
-	alert("Painiketta klikattiin!");
- });
+/* sama 
+document.getElementById("press").addEventListener("click", hello () );
+
+function hello () {
+		document.getElementById("content").textContent = "Hei, maailma!";
+}*/
+
+
+//DOM 01B
+
+document.getElementById("button").addEventListener("click", click);
+function click() {
+	let counter = document.getElementById("counter");//знаходимо елемент по ID (той що буде змінюватись) i зберігаємо його в змінній
+	let value = parseInt(counter.textContent); // отримуємо поточне значення
+	counter.textContent = value + 1; //textContent повертає значення, яке знаходиться всередині цього елемента. 
+}
+
+//DOM 01C
+
+document.getElementById("button_increase").addEventListener("click", increase);
+function increase() {
+	let counter = document.getElementById("number");//знаходимо елемент по ID (той що буде змінюватись) i зберігаємо його в змінній
+	let value = parseInt(counter.textContent); // отримуємо поточне значення
+	counter.textContent = value + 1; //присвоюємо counter нове значенняю textContent повертає значення, яке знаходиться всередині цього елемента. 
+}
+
+document.getElementById("button_reduce").addEventListener("click", reduce);
+function reduce() {
+	let counter = document.getElementById("number");//знаходимо елемент по ID (той що буде змінюватись) i зберігаємо його в змінній
+	let value = parseInt(counter.textContent); // отримуємо поточне значення
+	counter.textContent = value - 1; //присвоюємо counter нове значенняю textContent повертає значення, яке знаходиться всередині цього елемента. 
+}
+
+
+
