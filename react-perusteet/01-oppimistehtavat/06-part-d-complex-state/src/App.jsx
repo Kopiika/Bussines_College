8dd53a0664b80761d89fffe7a89e6ba3bdd4f881
Eleonora <eleonora.kopiika@gmail.com>
2025-08-21 13:57:14 +0300

    tein osa1-d "Complex state"

--- react-perusteet/01-oppimistehtavat/06-part-d-complex-state/src/App.jsx
+++ react-perusteet/01-oppimistehtavat/06-part-d-complex-state/src/App.jsx
@@ -1,35 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+  const App = () => {
+    const [clicks, setClicks] = useState({
+      left: 0, right: 0
+    })
+  
+    const handleLeftClick = () => {
+      setClicks({...clicks, left: clicks.left + 1})
+    }
+  
+    const handleRightClick = () => {
+      setClicks({...clicks, right: clicks.right + 1})
+    }
+  
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+    </div>
+    )
+  }
 
 export default App
